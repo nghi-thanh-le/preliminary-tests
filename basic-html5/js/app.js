4cdb72630dc62dbfbbfb96ef1230d35c6c98036b
Nghi <nghi.lethanh@outlook.com>
2017-01-11 00:05:50 +0200

    Basic Html - Sort by field

--- basic-html5/js/app.js
+++ basic-html5/js/app.js
@@ -3,28 +3,40 @@
 var myApp = angular.module('myApp', ['ngAnimate', 'ui.bootstrap', 'ngMessages']);
 
 myApp.controller('myController', function($scope, $uibModal, $log) {
+        $scope.sortType = ''; // set the default sort type
+        $scope.sortReverse = false; // set the default sort order
+        $scope.resetSort = function () {
+            $scope.sortType = '';
+            $scope.sortReverse = false;
+        };
+
         $scope.newUser = {
+            id: -1,
             name: '',
             gender: '',
             age: 0
         };
 
         $scope.users = [{
+            id: 0,
             name: 'John Doe',
             gender: 'Male',
             age: 32,
             edit: true
         }, {
+            id: 1,
             name: 'Diana Barry',
             gender: 'Female',
             age: 28,
             edit: true
         }, {
+            id: 2,
             name: 'Bruce Wayne',
             gender: 'Male',
             age: 46,
             edit: true
         }, {
+            id: 3,
             name: 'Jane Porter',
             gender: 'Female',
             age: 19,
@@ -35,12 +47,14 @@ myApp.controller('myController', function($scope, $uibModal, $log) {
             // check to make sure the form is completely valid
             if (isValid) {
                 var user = angular.copy($scope.newUser);
+                user.id = $scope.users.length;
                 user.edit = true;
                 $scope.users.push(user);
+                console.log($scope.users);
             }
         };
 
-        $scope.removeUser = function(index) {
+        $scope.removeUser = function(userId) {
             var modalInstance = $uibModal.open({
                 animation: true,
                 ariaLabelledBy: 'modal-title',
@@ -51,6 +65,9 @@ myApp.controller('myController', function($scope, $uibModal, $log) {
             });
 
             modalInstance.result.then(function(selectedItem) {
+                var index = $scope.users.findIndex(function (user) {
+                    return user.id == userId;
+                });
                 $scope.users.splice(index, 1);
             }, function() {
                 $log.info('Modal dismissed at: ' + new Date());
